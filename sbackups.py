13568f1a565d91bb9c78b8d81c36d3415b3c5bfb
ustasam <samonov@bk.ru>
2018-05-10 02:43:38 +0400

    backup complete

--- sbackups.py
+++ sbackups.py
@@ -18,27 +18,37 @@ import re
 import shutil
 import argparse
 
+# ### Config section ###########################################################################################
 
-# # Config section ###########################################################################################
+default = {
+    'path': r"\\oaovolga\users_backups\!hostname!",
 
-skip = [r'RECYCLE.BIN', r'\.tmp$', r'\.log$', r'Thumbs\.db$', r'desctop\.ini$', r'\.lnk$',
-        r'event-log$', r'\.mp3$', r'\.wav$', r'\.ogg$', r'\.mkv$', r'\.iso$']
+    'items': [{'item': "!my_documents!", 'path': ""},  # TODO: remove 'path'
+              {'item': "!desktop!", 'path': ""},
+              {'item': "!disks!", 'path': ""}],
 
-skip_compress = ['.png', '.jpg', '.wmf', '.avi', '.mpg', '.mov', '.mp3', '.ogg', '.mkv', '.fla', '.swf', '.rar',
-                 '.arj', '.arc', '.targz', '.7z', '.gz', '.bz', '.zip', '.tif', '.gif', '.dt', '.7zbk', '.iso']
+    'skip': [r'RECYCLE.BIN', r'\.tmp$', r'\.log$', r'Thumbs\.db$', r'desctop\.ini$', r'\.lnk$',
+             r'event-log$', r'\.mp3$', r'\.wav$', r'\.ogg$', r'\.mkv$', r'\.iso$'],
 
-log_file = "sbackups.log"
+    'skip_compress': ['.png', '.jpg', '.wmf', '.avi', '.mpg', '.mov', '.mp3', '.ogg', '.mkv',
+                      '.fla', '.swf', '.rar', '.arj', '.arc', '.targz', '.7z', '.gz', '.bz',
+                      '.zip', '.tif', '.gif', '.dt', '.7zbk', '.iso'],
 
-config = {
+    'log_file': "sbackups.log",
 
-    'default': {
-        'path': r"\\oaovolga\users_backups\!hostname!",
-        'items': [{'item': "!my_documents!", 'path': ""},
-                  {'item': "!desktop!", 'path': ""},
-                  {'item': "!disks!", 'path': ""}]
-    },
+    'compress': True,
+    'compress_extension': ".BAK.zip",
+    'file_time_format': '%d.%m.%Y_%H_%M_%S',
 
-    'oaovolga': {
+    'log_format': r'%(asctime)s %(levelname)-8s %(message)s',
+    'log_dateformat': r'%Y-%m-%d %H:%M:%S',
+
+    'mount': False
+    }
+
+host_config = {
+
+    'oaovolga': {  # oaovolga host config
         'path': "P:\\",
         'items': [{'item': "C:\\Users\\Администратор.OAOVOLGABAL\\Desktop", 'path': ""},
                   {'item': "C:\\Users\\Администратор.OAOVOLGABAL\\Documents", 'path': ""},
@@ -46,41 +56,29 @@ config = {
                   {'item': "J:\\Docs", 'path': ""},
                   {'item': "I:\\Data\\Buhgalteria", 'path': ""}
                   ],
-        'skip': skip + [r'\Volga\VOLGADB'],
-        'skip_compress': skip_compress,
-        'unmount': True
-    },
+        'skip': default['skip'] + [r'\Volga\VOLGADB'],
+        'mount': True
+        },
 
-    'gtx': {
-        'path': r"J:\my_backup",
-        'items': [{'item': "D:\\", 'path': ""},
-                  {'item': r"C:\Users\ustas\Documents", 'path': ""}],
-        'skip': skip,
-        'skip_compress': skip_compress,
-        'unmount': True
+    'gtx': {  # gtx host config
+        'path': r'J:\my_new_backup',
+        'items': [{'item': r'J:\james\small\_Chess', 'path': ""},
+                  {'item': r'C:\Users\ustas\Documents', 'path': ""}]
+        }
     }
 
-}
-
-compress = True
-compression_extension = ".BAK.zip"
-file_time_format = '%d.%m.%Y_%H_%M_%S'
-
-log_format = '%(asctime)s %(levelname)-8s %(message)s'
-log_dateformat = '%Y-%m-%d %H:%M:%S'
-
 ##############################################################################################################
+# TODO noprevious, nocompress,  nolog
 
-debug = True
 
+def backup_host(config):  # , items, backup_path, skip, skip_compress_items):
 
-def backup_host(hostname, items, backup_path, skip, skip_compress_items):
+    if config['path'] == "":
+        config['path'] = default['path']
 
-    logging.info("Backup host " + hostname + " started.")
+    for item in config['items']:
 
-    for item in items:
-
-        bak_path = backup_path
+        bak_path = config['path']
         if item['path'] == "":
             bak_path = os.path.join(bak_path, os.path.basename(item['item']))
         elif item['path'] == "-":
@@ -88,34 +86,31 @@ def backup_host(hostname, items, backup_path, skip, skip_compress_items):
         else:
             bak_path = item['path']
 
+        # TODO: if quiet:
         logging.info("Current backup item: " + item['item'] + " to " + bak_path)
 
         for root, dirs, files in os.walk(item['item']):
             for name in files:
 
                 skip = False
-                for skip_item in skip:
+                for skip_item in config['skip']:
                     if bool(re.search(skip_item, os.path.join(root, name), re.IGNORECASE)):
                         skip = True
 
-                item_compress = compress
-                for skip_compress_item in skip_compress_items:
-                    if name.lower().endswith(skip_compress_item):
+                item_compress = config['compress']
+                for skip_compress in config['skip_compress']:
+                    if name.lower().endswith(skip_compress):
                         item_compress = False
 
                 if not skip:
-
-                    logging.info("File : " + os.path.join(root, name))
-
                     try:
                         backup_file(root, name, bak_path, item['item'], item_compress)
+                        pass
                     except Exception, e:
                         logging.error("Error processing file " + os.path.join(root, name) + " " + str(e))
                 else:
                     # logging.info("Skipped - " + os.path.join(root, name))
                     pass
-
-    logging.info("Backup host " + hostname + " completed.")
 
 
 def backup_file(root, name, bkpath, bkroot, compress=True):
@@ -132,64 +127,77 @@ def backup_file(root, name, bkpath, bkroot, compress=True):
 
     src_filename_mtime = os.path.getmtime(src_filename)
 
-    if not debug:
+    if os.path.isfile(bak_filename):
 
-        if os.path.isfile(bak_filename):
+        bak_filename_mtime = os.path.getmtime(bak_filename)
+        if abs(bak_filename_mtime - src_filename_mtime) < 0.0001:
+            # print "Same modification time: " + src_filename
+            return  # has no changed
 
-            bak_filename_mtime = os.path.getmtime(bak_filename)
-            if abs(bak_filename_mtime - src_filename_mtime) < 0.0001:
-                # print "Same modification time: " + src_filename
-                return  # has no changed
+        mtime_str = time.strftime('%d.%m.%Y_%H_%M_%S', time.localtime(bak_filename_mtime))
+        new_name = bak_filename_c + "-" + mtime_str + (".BAK.zip" if compress else "")
 
-            mtime_str = time.strftime('%d.%m.%Y_%H_%M_%S', time.localtime(bak_filename_mtime))
-            new_name = bak_filename_c + "-" + mtime_str + (".BAK.zip" if compress else "")
+        if os.path.isfile(new_name):
+            os.remove(new_name)
+        os.rename(bak_filename, new_name)
 
-            if os.path.isfile(new_name):
-                os.remove(new_name)
-            os.rename(bak_filename, new_name)
+    logging.info("File : " + os.path.join(root, name))
 
-        if compress:
-            zip_file = zipfile.ZipFile(bak_filename, 'w', zipfile.ZIP_DEFLATED)
-            zip_file.write(src_filename, name)
-            zip_file.close()
-            shutil.copystat(src_filename, bak_filename)
-        else:
-            shutil.copy2(src_filename, bak_filename)
+    if compress:
+        zip_file = zipfile.ZipFile(bak_filename, 'w', zipfile.ZIP_DEFLATED)
+        zip_file.write(src_filename, name)
+        zip_file.close()
+        shutil.copystat(src_filename, bak_filename)
+    else:
+        shutil.copy2(src_filename, bak_filename)
 
 
 def backup(nocompress=False, noprevious=False):
-    # mount
-    print "backup"
-    pass
+    # TODO: mount
 
+    config = get_config()
 
-def complicated():
-    pass
+    logging.info("Backup host " + socket.gethostname() + " started.")
+
+    backup_host(config)
+
+    logging.info("Backup host " + socket.gethostname() + " completed.")
 
 
 def restore():
+    # config = get_config()
     pass
 
 
 def clean_versions():
+    # config = get_config()
     pass
 
 
 def mark_deleted():
+    # config = get_config()
+    pass
+
+
+def complicated():
+    # config = get_config()
     pass
 
 
 def get_config():
     hostname = socket.gethostname()
 
-    # host configuration
-    host_conf = config['default']
-    if hostname in config:
-        host_conf = config[hostname]
-    elif hostname.lower() in config:
-        host_conf = config[hostname.lower()]
-    elif hostname.upper() in config:
-        host_conf = config[hostname.upper()]
+    host_conf = default
+    if hostname in host_config:
+        host_conf = host_config[hostname]
+    elif hostname.lower() in host_config:
+        host_conf = host_config[hostname.lower()]
+    elif hostname.upper() in host_config:
+        host_conf = host_config[hostname.upper()]
+
+    for key, value in default.iteritems():
+        if key not in host_conf:
+            host_conf[key] = value
 
     return host_conf
 
@@ -197,7 +205,6 @@ def get_config():
 if __name__ == "__main__":
 
     print ""
-
     if len(sys.argv) == 1:
         sys.argv.append("backup")  # default command
 
@@ -210,8 +217,6 @@ if __name__ == "__main__":
                                default=False, help='Skip compress action')
     backup_parser.add_argument('--noprevious', action='store_true',
                                default=False, help='Delete previous versions')
-    backup_parser.add_argument('--debug', '-db', action='store_true',
-                               default=False, help='No file operations')
 
     restore_parser = subparsers.add_parser('restore', help='Restore operation')
     restore_parser.add_argument('--target', '-t', action='store', help='Restory target directory')
@@ -235,27 +240,23 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if debug:
-        print args
-
     # log configuration
+    config = get_config()
 
-    conf = get_config()
+    if not os.path.isabs(config['log_file']):
+        config['log_file'] = os.path.join(config['path'], config['log_file'])
 
-    conf_log_file = log_file
-    if "log_file" in conf:
-        conf_log_file =  conf["log_file"]
-    if not os.path.isabs(conf_log_file):
-        conf_log_file = os.path.join(conf['path'], conf_log_file)
+    if not os.path.exists(os.path.dirname(config['log_file'])):
+        os.makedirs(os.path.dirname(config['log_file']))
 
     logging.basicConfig(
-        filename=conf_log_file,
+        filename=config['log_file'],
         level=logging.DEBUG,
-        datefmt=log_dateformat,
-        format=log_format)
+        datefmt=config['log_dateformat'],
+        format=config['log_format'])
 
     ch = logging.StreamHandler()
-    ch.setFormatter(logging.Formatter(log_format))
+    ch.setFormatter(logging.Formatter(config['log_format']))
     logging.getLogger().addHandler(ch)
 
     logging.info("")
@@ -264,15 +265,17 @@ if __name__ == "__main__":
     # execute command operation
 
     if args.command == "backup":
-        debug = args.debug
         backup(nocompress=args.nocompress, noprevious=args.noprevious)
 
     elif args.command == "restore":
         restore()
+
     elif args.command == "clean":
         clean_versions()
+
     elif args.command == "mark":
         mark_deleted()
+        
     elif args.command == "complicated":
         backup()
         clean_versions()
